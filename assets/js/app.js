cd318c95e7e95f778f852488f9c2fde1155c8bf2
Dynodzzo <dynodzoo@gmail.com>
2013-10-01 21:58:14 +0200

    Interface design nearly done. JS functions pre-defined.

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	// Handle the click on an account
+	$('.app-content-data .app-content-account .account').click(function() {
+		// console.log($(this));
+	});
+
+	// Handle the click on an objective name
+	$('.app-content-data .app-content-objective .objective .objective-header .objective-label').click(function() {
+		// console.log($(this));
+	});
+
+	// Handle the click on an objective amount
+	$('.app-content-data .app-content-objective .objective .objective-postfix .objective-amount').click(function() {
+		// console.log($(this));
+	});
+
+	// Handle the click on an objective bar part
+	$('.app-content-data .app-content-objective .objective .objective-progress .objective-meter').click(function() {
+		// console.log($(this));
+	});
+});
